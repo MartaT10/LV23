914d6e85105385420d0ef60a66307bee9e8541db
MartaT10 <martatopolnjak2@gmail.com>
2026-04-13 11:03:19 +0200

    Bibidi

---
+++ LV23.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <queue>
+#include <random>
+using namespace std;
+
+void a_operacije();
+void b_imena();
+
+int main()
+{
+	//Marta Topolnjak, 1.RM
+
+	//a_operacije();
+	//b_imena();
+}
+
+void a_operacije() {
+	int n; cin >> n;
+	int a;
+	queue<int> red;
+
+	for (int i = 0;i < n;i++) {
+		a = rand() % 101;
+		red.push(a);
+	}
+
+	for (int i = 0;i < n;i++) {
+		if (red.front() > 99 && red.front() < 1000) {
+			cout << red.front() << " ";
+		}
+		red.pop();
+	}
+}
+
+void b_imena() {
+	queue<string> imena;
+	string ime; cin >> ime;
+
+	while (ime != "Kraj") {
+		imena.push(ime);
+		cin >> ime;
+	}
+
+	int dulj = imena.size();
+	cout << dulj << ": ";
+	for (int i = 0;i < dulj;i++) {
+		cout << imena.front()[0] << ". ";
+		imena.pop();
+	}
+}
